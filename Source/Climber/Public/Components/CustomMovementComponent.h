07043bf85ee5655952bee8be640eba987742f516
Osman Fatih Çakır <cakirosmanfatih@gmail.com>
2024-03-04 22:43:50 +0300

    helper trace functions

--- Source/Climber/Public/Components/CustomMovementComponent.h
+++ Source/Climber/Public/Components/CustomMovementComponent.h
@@ -7,11 +7,42 @@
 #include "CustomMovementComponent.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class CLIMBER_API UCustomMovementComponent : public UCharacterMovementComponent
 {
-	GENERATED_BODY()
-	
+  GENERATED_BODY()
+
+public:
+  virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+#pragma region ClimbTraces
+
+  TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+  FHitResult DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+
+#pragma endregion
+
+#pragma region ClimbCore
+
+  void TraceClimbableSurfaces();
+  void TraceFromEyeHeight(float TraceDistance, float TraceStartOffset = 0.0f);
+
+#pragma endregion
+
+#pragma region ClimbVariables
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+  TArray<TEnumAsByte<EObjectTypeQuery> > ClimbableSurfaceTraceTypes;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+  float ClimbCapsuleTraceRadius = 50.f;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+  float ClimbCapsuleTraceHalfHeight = 72.f;
+
+#pragma endregion
 };
